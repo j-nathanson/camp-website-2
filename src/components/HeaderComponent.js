b8fc367ca9a9f536d39927adcdf34c9aeee2c24f
jacobnathanson <jacobnath93@gmail.com>
2021-12-22 16:51:37 -0500

    React Router Parameters

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -19,7 +19,6 @@ class Header extends Component {
         });
     }
     render() {
-        console.log(this.toggleNav);
         return (
             <React.Fragment>
                 <Jumbotron fluid>
