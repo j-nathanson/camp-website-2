b78062db7403d8e2910fd217712e57e28f375790
jacobnathanson <jacobnath93@gmail.com>
2022-01-03 22:29:17 -0500

    campsite info controlled form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 const required = val => val && val.length;
-const maxLength = len => val => !val || (val.length <= len)
+const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
 const isNumber = val => !isNaN(+val);
 const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
